7bba4bf9893508341dc0aec367ef3a276ca32f0b
Carl Hostetler <carl.hostetler@gmail.com>
2022-04-06 20:23:05 -0700

    Make it repeatable

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
+resource "random_id" "id" {
+	  byte_length = 8
+}
+
 resource "google_project" "my_project" {
   name       = var.project
-  project_id = var.project
+  project_id = "${var.project}-${random_id.id.hex}"
 }
