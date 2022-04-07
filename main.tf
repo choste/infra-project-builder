ce0a132f9eb61ea68c0496139e2c0bcdac47a6f7
Carl Hostetler <carl.hostetler@gmail.com>
2022-04-06 21:23:15 -0700

    Add Subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "google_compute_network" "vpc_network" {
-  name = "dev-network"
+  name                    = "dev-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-a"
+  ip_cidr_range = "10.0.0.0/22"
+  network       = google_compute_network.vpc_network.id
 }
