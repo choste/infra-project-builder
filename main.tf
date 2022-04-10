ae2371d519ba07baf4d9bfb3ddf4fc9936875796
Carl Hostetler <carl.hostetler@gmail.com>
2022-04-10 09:51:16 -0700

    Add private google access

--- main.tf
+++ main.tf
@@ -4,7 +4,9 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet-a"
-  ip_cidr_range = "10.2.0.0/16"
-  network       = google_compute_network.vpc_network.id
+  name                     = "subnet-a"
+  ip_cidr_range            = "10.1.0.0/16"
+  network                  = google_compute_network.vpc_network.id
+  private_ip_google_access = true
+  private_ipv6_google_access = true
 }
